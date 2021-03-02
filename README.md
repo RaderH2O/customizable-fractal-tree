8c82481c36393b996061cdf06c3667c011313ac2
RaderH2O <raderh2ogt@gmail.com>
2021-03-02 22:24:31 +0330

    Added a thickness slider

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Either start a web server (with anything , literally) or just open the index.html file in your browser
 
 ## What are the sliders?
-(From the left) The first one is the angle between every branch with the previous branch, the second one is the size of the tree and the last one is the maximum length of the branches (i.e the count of branches)
+(From the left) The first one is the angle between every branch with the previous branch, the second one is the size of the tree, the third one is the maximum length of the branches (i.e the count of branches), the fourth one is for the thickness of branches
 
 ## How did you make it?
 I made it with the p5.js library which is a really good library for anything related to graphics
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let branchLength = 150;
 let angle;
 let maxLength;
+let thickness;
+let thicknessSlider;
 let angleSlider;
 let maxLengthSlider;
 
@@ -9,6 +11,7 @@ function setup() {
     angleSlider = createSlider(0.01, TWO_PI, PI/8, 0.01);
     lengthSlider = createSlider(10, 300, 150, 1);
     maxLengthSlider = createSlider(4, 10, 4, 0.1);
+    thicknessSlider = createSlider(0.5, 50, 10, 0.5);
 }
 
 function draw() {
@@ -16,10 +19,11 @@ function draw() {
     angle = angleSlider.value();
     branchLength = lengthSlider.value();
     maxLength = map(maxLengthSlider.value(), 4, 10, 50, 4);
+    thickness = thicknessSlider.value();
     translate(width/2, height);
     stroke(255);
     strokeWeight(1);
-    branch(branchLength, 10);
+    branch(branchLength, thickness);
 }
 
 function branch(len, stroke) {
